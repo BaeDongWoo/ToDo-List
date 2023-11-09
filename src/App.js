7a6244f5d365ac787d4a558574cf5bd75757a7b0
BaeDongWoo <ehddn5410@naver.com>
2023-11-10 04:07:47 +0900

    feat:react-router 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './components/login/LoginPage';
+import MainPage from './components/main/MainPage';
 
 function App() {
   return (
-    <div>
-      <LoginPage />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/MainPage" element={<MainPage />} />
+      </Routes>
+    </Router>
   );
 }
 
