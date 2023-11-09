d0df3e1092e37b7c9aaa81e5465c418d18488607
BaeDongWoo <ehddn5410@naver.com>
2023-11-10 04:06:11 +0900

    feat:로그인을 위한 서버 호출

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,14 +1,54 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './LoginForm.css';
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 const LoginForm = () => {
+  const [userId, setUserId] = useState('');
+  const [password, setPassword] = useState('');
+  const nav = useNavigate();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:3005/login', {
+        userId,
+        password,
+      });
+      console.log(response);
+      // 서버에서의 응답을 확인하고 처리
+      if (response.data === '확인') {
+        console.log('로그인 성공!');
+        nav('/MainPage');
+      } else {
+        console.log('로그인 실패');
+        alert('아이디 또는 비밀번호가 다릅니다.');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
   return (
     <div className="login-container">
       <div className="login-wrapper">
         <h2>Login</h2>
-        <form method="post" id="login-form">
-          <input type="text" placeholder="아이디를 입력해 주세요"></input>
-          <input type="password" placeholder="비밀번호를 입력해 주세요"></input>
-          <label for="remember-check"></label>
+        <form method="get" id="login-form" onSubmit={handleLogin}>
+          <input
+            type="text"
+            placeholder="아이디를 입력해 주세요"
+            value={userId}
+            onChange={(e) => {
+              setUserId(e.target.value);
+            }}
+          ></input>
+          <input
+            type="password"
+            placeholder="비밀번호를 입력해 주세요"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          ></input>
+          <label htmlFor="remember-check"></label>
           <input type="submit" value="로그인"></input>
           <input type="button" value="회원가입"></input>
         </form>
