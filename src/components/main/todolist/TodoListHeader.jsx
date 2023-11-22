aded176f7542b3b910e7fa50fb3ff0a994ed99d5
BaeDongWoo <ehddn5410@naver.com>
2023-11-22 23:11:44 +0900

    feat:입력 이벤트 추가 리스트 추가

--- src/components/main/todolist/TodoListHeader.jsx
+++ src/components/main/todolist/TodoListHeader.jsx
@@ -1,13 +1,32 @@
-const TodoListHeader = ({ date }) => {
+import { useState } from 'react';
+
+const TodoListHeader = ({ date, todoList, setTodoList }) => {
+  const [inputTodo, setInputTodo] = useState('');
+  const todoListHandler = () => {
+    setTodoList([...todoList, { title: inputTodo }]);
+    setInputTodo('');
+  };
+  const onChangeHandler = (e) => {
+    setInputTodo(e.target.value);
+  };
   return (
     <div className="todo-header">
       <div className="select-date">
         {date.getFullYear()}년 {date.getMonth() + 1}월 {date.getDate()}일
       </div>
       <div className="add-todo">
-        <input type="text" placeholder="할일을 입력해주세요"></input>
+        <input
+          type="text"
+          placeholder="할일을 입력해주세요"
+          onChange={onChangeHandler}
+          value={inputTodo}
+        ></input>
         <div className="add-todo-text">일정 추가</div>
-        <button type="button" className="add-todo-button">
+        <button
+          type="button"
+          className="add-todo-button"
+          onClick={todoListHandler}
+        >
           +
         </button>
       </div>
