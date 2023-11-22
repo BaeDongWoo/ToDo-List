7ea0675cc09f73f517ea143d98510665e9879776
BaeDongWoo <ehddn5410@naver.com>
2023-11-23 01:18:00 +0900

    feat:리스트 삭제추가

--- src/components/main/todolist/PrintTodo.jsx
+++ src/components/main/todolist/PrintTodo.jsx
@@ -1,12 +1,22 @@
 const PrintTodo = ({ todoList, setTodoList }) => {
+  const deleteTodolist = (id) => {
+    setTodoList((todoList) => todoList.filter((todo) => todo.id !== id));
+  };
   return (
     <div className="todo-list">
       <ul>
         {todoList.map((todo, idx) => (
-          <li className="todo" key={idx}>
+          <li className="todo" key={idx} id={idx}>
             <input type="checkbox" className="checkbox"></input>
-            <div className="todo-title">{todo.title}</div>
-            <button className="todo-delete-btn">x</button>
+            <div className="todo-title" value={todo.title}>
+              {todo.title}
+            </div>
+            <button
+              className="todo-delete-btn"
+              onClick={() => deleteTodolist(todo.id)}
+            >
+              x
+            </button>
           </li>
         ))}
       </ul>
