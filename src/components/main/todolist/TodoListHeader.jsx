3657bcecef8ce685f7cb4650c256bf805409ffb1
BaeDongWoo <ehddn5410@naver.com>
2023-11-22 22:52:33 +0900

    feat:입력폼 추가

--- src/components/main/todolist/TodoListHeader.jsx
+++ src/components/main/todolist/TodoListHeader.jsx
@@ -5,8 +5,11 @@ const TodoListHeader = ({ date }) => {
         {date.getFullYear()}년 {date.getMonth() + 1}월 {date.getDate()}일
       </div>
       <div className="add-todo">
+        <input type="text" placeholder="할일을 입력해주세요"></input>
         <div className="add-todo-text">일정 추가</div>
-        <button className="add-todo-button">+</button>
+        <button type="button" className="add-todo-button">
+          +
+        </button>
       </div>
     </div>
   );
