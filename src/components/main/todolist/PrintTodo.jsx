0e478d230a8a7a7a95d4067736034a3542e4abe8
BaeDongWoo <ehddn5410@naver.com>
2023-11-23 20:02:14 +0900

    feat:완료 일정 체크

--- src/components/main/todolist/PrintTodo.jsx
+++ src/components/main/todolist/PrintTodo.jsx
@@ -2,13 +2,30 @@ const PrintTodo = ({ todoList, setTodoList }) => {
   const deleteTodolist = (id) => {
     setTodoList((todoList) => todoList.filter((todo) => todo.id !== id));
   };
+  const toggleChecked = (id) => {
+    setTodoList(
+      todoList.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
+  };
   return (
     <div className="todo-list">
       <ul>
         {todoList.map((todo, idx) => (
           <li className="todo" key={idx} id={idx}>
-            <input type="checkbox" className="checkbox"></input>
-            <div className="todo-title" value={todo.title}>
+            <input
+              type="checkbox"
+              className="checkbox"
+              checked={todo.checked}
+              onChange={() => toggleChecked(todo.id)}
+            ></input>
+            <div
+              className={
+                todo.checked === false ? 'todo-title' : 'todo-title-checked'
+              }
+              value={todo.title}
+            >
               {todo.title}
             </div>
             <button
