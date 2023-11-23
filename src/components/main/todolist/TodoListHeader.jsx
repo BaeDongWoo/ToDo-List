9f6074e3afca2c7c9eee72518ddf1151103e5fc2
BaeDongWoo <ehddn5410@naver.com>
2023-11-23 20:03:23 +0900

    feat:checked속성 추가

--- src/components/main/todolist/TodoListHeader.jsx
+++ src/components/main/todolist/TodoListHeader.jsx
@@ -3,12 +3,13 @@ import { useState } from 'react';
 const TodoListHeader = ({ date, todoList, setTodoList }) => {
   const [inputTodo, setInputTodo] = useState('');
   const todoListHandler = () => {
-    const todos = todoList.concat({ title: inputTodo });
+    const todos = todoList.concat({ title: inputTodo, checked: false });
     setTodoList(
       todos.map((todo, idx) => {
         return { ...todo, id: idx };
       })
     );
+    console.log(todoList);
     setInputTodo('');
   };
   const onChangeHandler = (e) => {
