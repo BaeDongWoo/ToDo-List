9fb4909d7c370b5f98f131648933b5f368ed3b81
BaeDongWoo <ehddn5410@naver.com>
2023-11-24 16:36:07 +0900

    feat:DnD라이브러리로 변경

--- src/components/main/todolist/PrintTodo.jsx
+++ src/components/main/todolist/PrintTodo.jsx
@@ -1,90 +1,91 @@
-import { useState } from 'react';
+import React from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 const PrintTodo = ({ todoList, setTodoList }) => {
-  const [grab, setGrab] = useState(null);
-  const _onDragOver = (e) => {
-    e.preventDefault();
-  };
-
-  const _onDragStart = (e) => {
-    setGrab(e.target);
-    e.target.classList.add('grabbing');
-    e.dataTransfer.effectAllowed = 'move';
-    e.dataTransfer.setData('text/html', e.target);
-  };
-
-  const _onDragEnd = (e) => {
-    e.target.classList.remove('grabbing');
-    e.dataTransfer.dropEffect = 'move';
-  };
-
-  const _onDrop = (e) => {
-    let grabPosition = grab.id;
-    let targetPosition = e.target.id;
-    let _list = [...todoList];
-    _list[grabPosition] = _list.splice(
-      targetPosition,
-      1,
-      _list[grabPosition]
-    )[0];
-
-    setTodoList(
-      _list.map((todo, idx) => {
-        return { ...todo, id: idx };
-      })
-    );
-    console.log(todoList);
-  };
-
   const deleteTodolist = (id) => {
-    setTodoList((todoList) => todoList.filter((todo) => todo.id !== id));
+    setTodoList((todos) => todos.filter((todo) => todo.id !== id));
   };
+
   const toggleChecked = (id) => {
-    setTodoList(
-      todoList.map((todo) =>
+    setTodoList((todos) =>
+      todos.map((todo) =>
         todo.id === id ? { ...todo, checked: !todo.checked } : todo
       )
     );
   };
+
+  const onDragEnd = (result) => {
+    console.log(result);
+    if (!result.destination) {
+      return;
+    }
+
+    const sourceIndex = result.source.index;
+    const destinationIndex = result.destination.index;
+
+    setTodoList((todos) => {
+      console.log(todos);
+      let updatedTodos = [...todos];
+      updatedTodos[destinationIndex] = updatedTodos.splice(
+        sourceIndex,
+        1,
+        updatedTodos[destinationIndex]
+      )[0];
+      return updatedTodos.map((todo, index) => ({ ...todo, id: index }));
+    });
+  };
+
   return (
-    <div className="todo-list">
-      <ul>
-        {todoList.map((todo, idx) => (
-          <li
-            className="todo"
-            key={idx}
-            id={idx}
-            draggable
-            onDragOver={_onDragOver}
-            onDragStart={_onDragStart}
-            onDragEnd={_onDragEnd}
-            onDrop={_onDrop}
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Droppable droppableId="todoList">
+        {(provided) => (
+          <ul
+            className="todo-list"
+            {...provided.droppableProps}
+            ref={provided.innerRef}
           >
-            <input
-              type="checkbox"
-              className="checkbox"
-              checked={todo.checked}
-              onChange={() => toggleChecked(todo.id)}
-            ></input>
-            <div
-              className={
-                todo.checked === false ? 'todo-title' : 'todo-title-checked'
-              }
-              value={todo.title}
-              id={idx}
-            >
-              {todo.title}
-            </div>
-            <button
-              className="todo-delete-btn"
-              onClick={() => deleteTodolist(todo.id)}
-            >
-              x
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
+            {todoList.map((todo, index) => (
+              <Draggable
+                key={todo.id}
+                draggableId={todo.id.toString()}
+                index={index}
+              >
+                {(provided) => (
+                  <li
+                    className="todo"
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <input
+                      type="checkbox"
+                      className="checkbox"
+                      checked={todo.checked}
+                      onChange={() => toggleChecked(todo.id)}
+                    />
+                    <div
+                      className={
+                        todo.checked ? 'todo-title-checked' : 'todo-title'
+                      }
+                    >
+                      {todo.title}
+                    </div>
+                    <button
+                      className="todo-delete-btn"
+                      onClick={() => deleteTodolist(todo.id)}
+                    >
+                      x
+                    </button>
+                  </li>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </ul>
+        )}
+      </Droppable>
+    </DragDropContext>
   );
 };
+
 export default PrintTodo;
