cd9c00737b40d10efdd39f2802557f45f22d614b
BaeDongWoo <ehddn5410@naver.com>
2023-11-27 22:01:31 +0900

    refactor:토글, 삭제 함수 분리

--- src/components/main/todolist/PrintTodo.jsx
+++ src/components/main/todolist/PrintTodo.jsx
@@ -1,19 +1,8 @@
 import React from 'react';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import Card from './Card';
-
+import './PrintTodo.css';
 const PrintTodo = ({ todoList, setTodoList }) => {
-  const deleteTodolist = (id) => {
-    setTodoList((todos) => todos.filter((todo) => todo.id !== id));
-  };
-
-  const toggleChecked = (id) => {
-    setTodoList((todos) =>
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo
-      )
-    );
-  };
   const onDragEnd = (result) => {
     console.log(result);
     if (!result.destination) {
