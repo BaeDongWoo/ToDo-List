4d823e97a023bc458677d00a96bbcdcd419334d9
BaeDongWoo <ehddn5410@naver.com>
2023-11-28 00:45:07 +0900

    feat:캘린더에 할일 목록 출력

--- src/components/main/calendar/CalendarBody.jsx
+++ src/components/main/calendar/CalendarBody.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react';
 import './CalendarBody.css';
-const CalendarBody = ({ date, setDate }) => {
+const CalendarBody = ({ date, setDate, allTodoList }) => {
   const [selectCell, setSelectCell] = useState(date.getDate());
   const year = date.getFullYear();
   const month = date.getMonth();
-  const selectHandeler = (e) => {
-    const cell = Number(e.target.innerText);
-    setDate(new Date(year, month, cell));
-    setSelectCell(cell);
-    console.log(date.getDate());
+  const selectHandeler = (i) => {
+    setDate(new Date(year, month, i));
+    setSelectCell(i);
   };
   const setCalendar = () => {
     const daysInMonth = new Date(year, month + 1, 0).getDate();
@@ -25,25 +23,65 @@ const CalendarBody = ({ date, setDate }) => {
       key += 1;
     }
     for (let i = 1; i <= daysInMonth; i++) {
+      const tempDate = new Date(year, month, i);
+      const tempY = tempDate.getFullYear();
+      const tempM = tempDate.getMonth();
+      const tempD = tempDate.getDate();
+      const dateFormat = [tempY, tempM + 1, tempD].join('-');
+      const todoListForDate = allTodoList.find(
+        (item) => item.date === dateFormat
+      );
       if (selectCell === i) {
         calendar.push(
           <div
             className="week select"
             key={key}
-            onClick={selectHandeler}
-            value={22}
+            onClick={() => selectHandeler(i)}
+            value={tempD}
           >
-            {new Date(year, month, i).getDate()}
+            {tempD}
+            {todoListForDate && (
+              <ul className="all-todo-list">
+                {todoListForDate.todoList.map((todo) => (
+                  <li
+                    className={`${
+                      todo.checked ? 'all-todo-select' : 'all-todo'
+                    }`}
+                    key={todo.id}
+                  >
+                    {todo.title}
+                  </li>
+                ))}
+              </ul>
+            )}
           </div>
         );
       } else {
         calendar.push(
-          <div className="week" key={key} onClick={selectHandeler}>
-            {new Date(year, month, i).getDate()}
+          <div
+            className="week"
+            key={key}
+            onClick={() => selectHandeler(i)}
+            value={tempD}
+          >
+            {tempD}
+            {todoListForDate && (
+              <ul className="all-todo-list">
+                {todoListForDate.todoList.map((todo) => (
+                  <li
+                    className={`${
+                      todo.checked ? 'all-todo-select' : 'all-todo'
+                    }`}
+                    key={todo.id}
+                  >
+                    {todo.title}
+                  </li>
+                ))}
+              </ul>
+            )}
           </div>
         );
       }
-
       key += 1;
     }
     const daysInNextMonth = 7 - (calendar.length % 7);
