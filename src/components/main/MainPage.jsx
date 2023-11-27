0ce7c858dd26473dd1dbf180a12d73d6a981d44c
BaeDongWoo <ehddn5410@naver.com>
2023-11-27 22:06:03 +0900

    design:css 추가 및 변경

--- src/components/main/MainPage.jsx
+++ src/components/main/MainPage.jsx
@@ -1,8 +1,23 @@
+import Calendar from './calendar/Calendar';
+import TodoList from './todolist/TodoList';
+import UserProfile from './userprofile/UserProfile';
+import './MainPage.css';
+import { useState } from 'react';
+import Header from '../form/Header';
 const MainPage = () => {
+  const [date, setDate] = useState(new Date());
   return (
-    <>
-      <h1>메인페이지</h1>
-    </>
+    <div className="main-page">
+      <Header />
+      <div className="main">
+        <div className="profile-calendar">
+          <Calendar date={date} setDate={setDate} />
+        </div>
+        <div className="todo-form">
+          <TodoList date={date} />
+        </div>
+      </div>
+    </div>
   );
 };
 export default MainPage;
