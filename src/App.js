8a384d3bd15719408dea11b0d41c9ec600fe935f
BaeDongWoo <ehddn5410@naver.com>
2023-11-30 04:06:34 +0900

    feat:라우터 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import LoginPage from './components/login/LoginPage';
 import MainPage from './components/main/MainPage';
+import LoginForm from './components/login/LoginForm';
+import SingUp from './components/signup/SignUp';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/signUp" element={<SingUp />} />
         <Route path="/MainPage" element={<MainPage />} />
       </Routes>
     </Router>
