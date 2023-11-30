7b3c01e66b71af9570eab9282c0340ab4cc4504f
BaeDongWoo <ehddn5410@naver.com>
2023-11-30 20:03:21 +0900

    feat:아이디,비밀번호 유효성 검사 및 이벤트 추가

--- src/components/signup/SignUp.jsx
+++ src/components/signup/SignUp.jsx
@@ -1,40 +1,66 @@
 import { useNavigate } from 'react-router-dom';
 import Header from '../form/Header';
 import './SignUp.css';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Input from './Input';
 import axios from 'axios';
+import validation from './validation';
 const SignUp = () => {
   const [userName, setUserName] = useState('');
   const [userId, setUserId] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [isConfirmPwdId, setIsConfirmPwdId] = useState('confirm-password');
   const nav = useNavigate();
   const loginPageHandler = () => {
     nav('/login');
   };
+  const confirmPwdHandler = (value) => {
+    setConfirmPassword(value);
+    if (value === '' || password === '')
+      return setIsConfirmPwdId('confirm-password');
+    if (password !== value) {
+      setIsConfirmPwdId('confirm-password-failure');
+    } else setIsConfirmPwdId('confirm-password-success');
+  };
+  const PwdHandler = (value) => {
+    setPassword(value);
+    if (value === '' || confirmPassword === '')
+      return setIsConfirmPwdId('confirm-password');
+    if (confirmPassword !== value) {
+      setIsConfirmPwdId('confirm-password-failure');
+    } else setIsConfirmPwdId('confirm-password-success');
+  };
   const signUpHandler = async (e) => {
     e.preventDefault();
-    if (password !== confirmPassword)
-      return alert('비밀번호를 다시 확인해주세요');
-    try {
-      const response = await axios.post('http://localhost:3005/signUp', {
-        userName,
-        userId,
-        password,
-      });
-      console.log(response.data);
-      // 서버에서의 응답을 확인하고 처리
-      if (response.data === 1) {
-        // 아이디가 있음
-        alert('중복된 아이디 입니다.');
-      } else {
-        //아이디 없음
-        nav('/login');
-      }
-    } catch (error) {
-      console.error('Error:', error);
+    const val = new validation();
+    if (!val.validationId(userId)) {
+      return alert('아이디는 4글자 이상 12글자 이하 영어,숫자만 가능합니다.');
     }
+    if (!val.validationPwd(password)) {
+      return alert('비밀번호는 8글자 이상 영어,숫자만 가능합니다.');
+    }
+    if (!val.isCheckPwd(password, confirmPassword)) {
+      return alert('비밀번호를 다시 확인해주세요.');
+    }
+    // try {
+    //   const response = await axios.post('http://localhost:3005/signUp', {
+    //     userName,
+    //     userId,
+    //     password,
+    //   });
+    //   console.log(response.data);
+    //   // 서버에서의 응답을 확인하고 처리
+    //   if (response.data === 1) {
+    //     // 아이디가 있음
+    //     alert('중복된 아이디 입니다.');
+    //   } else {
+    //     //아이디 없음
+    //     nav('/login');
+    //   }
+    // } catch (error) {
+    //   console.error('Error:', error);
+    // }
   };
   return (
     <div className="signup-container">
@@ -59,16 +85,16 @@ const SignUp = () => {
           <Input
             title={'비밀번호'}
             typed={'password'}
-            id={'password'}
+            id={isConfirmPwdId}
             value={password}
-            onChangeEvent={setPassword}
+            onChangeEvent={PwdHandler}
           />
           <Input
             title={'비밀번호 확인'}
             typed={'password'}
-            id={'confirm-password'}
+            id={isConfirmPwdId}
             value={confirmPassword}
-            onChangeEvent={setConfirmPassword}
+            onChangeEvent={confirmPwdHandler}
           />
           <input type="submit" value="가입하기"></input>
           <input
