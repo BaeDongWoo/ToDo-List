8f6775b8f0861f2ddb257780f6ed020c14be3fdf
BaeDongWoo <ehddn5410@naver.com>
2023-12-01 00:23:49 +0900

    feat:userInfo store추가

--- src/components/reducer/Action.js
+++ src/components/reducer/Action.js
@@ -1,10 +1,15 @@
 export const SET_DATE = 'SET_DATE';
+export const USER_INFO = 'USER_INFO';
 export const SET_TODO_LIST = 'SET_TODO_LIST';
 export const SET_ALL_TODO_LIST = 'SET_ALL_TODO_LIST';
 
 export const setDate = (date) => ({
   type: SET_DATE,
   payload: date,
+});
+export const setUserInfo = (userInfo) => ({
+  type: USER_INFO,
+  payload: userInfo,
 });
 
 export const setTodoList = (todoList) => ({
--- src/components/reducer/Reducer.js
+++ src/components/reducer/Reducer.js
@@ -1,31 +1,22 @@
-import { SET_DATE, SET_TODO_LIST, SET_ALL_TODO_LIST } from './Action';
+import {
+  SET_DATE,
+  SET_TODO_LIST,
+  SET_ALL_TODO_LIST,
+  USER_INFO,
+} from './Action';
 
 const initialMainState = {
+  userInfo: JSON.parse(sessionStorage.getItem('userInfo')),
   date: new Date(),
   todoList: [],
-  allTodoList: [
-    {
-      date: '2023-11-27',
-      todoList: [
-        { title: '코테 풀기', checked: true, id: 0 },
-        { title: '리액트 강의 듣기', checked: false, id: 1 },
-        { title: '프로젝트 회의', checked: false, id: 2 },
-      ],
-    },
-    {
-      date: '2023-11-28',
-      todoList: [
-        { title: '정처기 공부하기', checked: false, id: 0 },
-        { title: '블로그 글쓰기', checked: true, id: 1 },
-        { title: '버그 수정하기', checked: false, id: 2 },
-      ],
-    },
-  ],
+  allTodoList: [],
 };
 const mainReducer = (state = initialMainState, action) => {
   switch (action.type) {
     case SET_DATE:
       return { ...state, date: action.payload };
+    case USER_INFO:
+      return { ...state, userInfo: action.payload };
     case SET_TODO_LIST:
       return { ...state, todoList: action.payload };
     case SET_ALL_TODO_LIST:
