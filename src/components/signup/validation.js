9802e06810cf016e1da1a061fe1374b5536e33cc
BaeDongWoo <ehddn5410@naver.com>
2023-11-30 20:02:27 +0900

    refactor:코드 수정

--- src/components/signup/validation.js
+++ src/components/signup/validation.js
@@ -1,15 +1,19 @@
-export const validationId = (id) => {
-  if (id.length < 4 && id.length > 12) return false;
-  if (!/^[A-Za-z0-9][A-Za-z0-9]*$/.test(id)) return false;
-  return true;
-};
-export const validationPwd = (pwd) => {
-  if (pwd.length < 8) return false;
-  if (!/^[A-Za-z0-9][A-Za-z0-9]*$/.test(pwd)) {
-    return false;
-  }
-  return true;
-};
-export const isCheckPwd = (pwd, confirmPwd) => {
-  if (pwd !== confirmPwd) return false;
-};
+class validation {
+  validationId = (id) => {
+    if (id.length < 4 || id.length > 12) return false;
+    if (!/^[A-Za-z0-9][A-Za-z0-9]*$/.test(id)) return false;
+    return true;
+  };
+  validationPwd = (pwd) => {
+    if (pwd.length < 8) return false;
+    if (!/^[A-Za-z0-9][A-Za-z0-9]*$/.test(pwd)) {
+      return false;
+    }
+    return true;
+  };
+  isCheckPwd = (pwd, confirmPwd) => {
+    if (pwd !== confirmPwd) return false;
+    return true;
+  };
+}
+export default validation;
