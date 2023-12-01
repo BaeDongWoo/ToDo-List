a79a8b7625b4afbd9435db73e7569dac1789c7e7
BaeDongWoo <ehddn5410@naver.com>
2023-12-01 18:52:00 +0900

    del:코드 삭제

--- Server.js
+++ Server.js
@@ -93,10 +93,6 @@ app.post('/saveTodo', (req, res) => {
 app.use((req, res) => {
   res.status(404).send('페이지를 찾을 수 없습니다.');
 });
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
-
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
