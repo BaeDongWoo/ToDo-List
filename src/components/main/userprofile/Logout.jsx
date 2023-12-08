f0e00cfb0759d1eadfeff1d25d6d6eff1a853a97
BaeDongWoo <ehddn5410@naver.com>
2023-12-08 19:05:48 +0900

    remove:userProfile

--- src/components/main/userprofile/Logout.jsx
+++
@@ -1,9 +0,0 @@
-import './Logout.css';
-const Logout = () => {
-  return (
-    <div className="logout">
-      <button className="logout-btn">logout</button>
-    </div>
-  );
-};
-export default Logout;
--- src/components/main/userprofile/UserName.jsx
+++
@@ -1,6 +0,0 @@
-import './UserName.css';
-const UserName = () => {
-  const name = '배추';
-  return <div className="user-name">이름 : {name}</div>;
-};
-export default UserName;
