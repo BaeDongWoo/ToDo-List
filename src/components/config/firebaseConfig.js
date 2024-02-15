9312e096f9498a5a1535d425a0f81383e75c42e4
BaeDongWoo <ehddn5410@naver.com>
2024-02-15 17:11:55 +0900

    feat:파이어베이스 스토어 추가

--- src/components/config/firebaseConfig.js
+++ src/components/config/firebaseConfig.js
@@ -1,10 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { config } from './config';
 import { getAuth } from 'firebase/auth';
-// import { getFirestore } from 'firebase/firestore';
-
+import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = config.firebaseConfigKey;
 const app = initializeApp(firebaseConfig);
-// const fireStore = getFirestore(app);
+const fireStore = getFirestore(app);
 const auth = getAuth(app);
-export { app, auth };
+export { auth, fireStore };
