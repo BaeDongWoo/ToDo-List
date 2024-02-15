ed9cf479528ae2f28de11c3037c37f3a6a1cf215
BaeDongWoo <ehddn5410@naver.com>
2024-02-15 17:10:47 +0900

    refactor:파이어베이스 이메일 로그인

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,16 +1,20 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './LoginForm.css';
 import { useState } from 'react';
-import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import Header from '../form/Header';
 import { useDispatch } from 'react-redux';
 import { setAllTodoList, setTodoList, setUserInfo } from '../reducer/Action';
 import { DateFormat } from '../form/DateFormat';
 import { useSelector } from 'react-redux';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth, fireStore } from '../config/firebaseConfig';
+import ErrorHandler from '../error/errorHander';
+import { collection, getDocs, query } from 'firebase/firestore';
 const LoginForm = () => {
   const date = useSelector((state) => state.date);
-  const [userId, setUserId] = useState('');
+  const userInfo = useSelector((state) => state.userInfo);
+  const [userEmail, setUserEmail] = useState('');
   const [password, setPassword] = useState('');
   const nav = useNavigate();
   const dispatch = useDispatch();
@@ -20,65 +24,16 @@ const LoginForm = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('http://localhost:3005/login', {
-        userId,
-        password,
-      });
-      // 서버에서의 응답을 확인하고 처리
-      if (response.data.userInfo) {
-        const userInfo = response.data.userInfo;
-        const header = response.headers.get('Authorization');
-        const token = header.slice(7);
-        dispatch(setUserInfo(userInfo));
-        sessionStorage.setItem('token', JSON.stringify(token));
-        const getData = async () => {
-          const response = await axios.post(
-            'http://localhost:3005/getTodoList',
-            {
-              userInfo,
-            },
-            {
-              headers: {
-                Authorization: token,
-              },
-            }
-          );
-          const dateList = response.data.dateList;
-          const allList = response.data.allTodoList;
-          const setData = [];
-          for (let i = 0; i < dateList.length; i++) {
-            const tempTodo = allList.filter(
-              (todo) => todo.todo_date === dateList[i].todo_date
-            );
-            const setList = tempTodo.map((todo, idx) => {
-              return {
-                title: todo.todo_title,
-                checked: todo.todo_checked === '0' ? false : true,
-                id: idx,
-              };
-            });
-            setData.push({ date: dateList[i].todo_date, todoList: setList });
-          }
-          dispatch(setAllTodoList(setData));
-          const dateFormat = DateFormat(date);
-          const todoListForDate = setData.find(
-            (item) => item.date === dateFormat
-          );
-          if (todoListForDate) {
-            dispatch(setTodoList(todoListForDate.todoList));
-            nav('/MainPage');
-          } else {
-            dispatch(setTodoList([]));
-            nav('/MainPage');
-          }
-        };
-        getData();
-        // nav('/MainPage');
-      } else {
-        alert('아이디 또는 비밀번호가 다릅니다.');
-      }
+      const user = await signInWithEmailAndPassword(auth, userEmail, password);
+      const uid = user.user.uid;
+      sessionStorage.setItem('userInfo', JSON.stringify(uid));
+      dispatch(setUserInfo(uid));
+      const q = query(collection(fireStore, 'users', uid, 'all_todo_list'));
+      const response = await getDocs(q);
+      // nav('/MainPage');
     } catch (error) {
-      console.log(error);
+      console.error(error);
+      ErrorHandler(error);
     }
   };
 
@@ -90,10 +45,10 @@ const LoginForm = () => {
         <form method="post" id="login-form" onSubmit={handleLogin}>
           <input
             type="text"
-            placeholder="아이디를 입력해 주세요"
-            value={userId}
+            placeholder="이메일을 입력해 주세요"
+            value={userEmail}
             onChange={(e) => {
-              setUserId(e.target.value);
+              setUserEmail(e.target.value);
             }}
           ></input>
           <input
