9f2ba5d0b35233f71a488b40d46972cb05c6bca3
BaeDongWoo <ehddn5410@naver.com>
2024-02-15 14:09:09 +0900

    feat:firebaseConfig 추가

---
+++ src/components/config/firebaseConfig.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+import { config } from './config';
+import { getAuth } from 'firebase/auth';
+// import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = config.firebaseConfigKey;
+const app = initializeApp(firebaseConfig);
+// const fireStore = getFirestore(app);
+const auth = getAuth(app);
+export { app, auth };
