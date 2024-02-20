e661c9021621c4806aabe9a4b9beff1900919386
BaeDongWoo <ehddn5410@naver.com>
2024-02-20 19:23:29 +0900

    feat:네이버 로그인 추가

---
+++ src/components/login/social/naver/NaverLogin.jsx
@@ -0,0 +1,62 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  setAllTodoList,
+  setDate,
+  setTodoList,
+  setUserInfo,
+} from '../../../reducer/Action';
+import { getData } from '../../../firebasestore/Data';
+import { useDispatch } from 'react-redux';
+import { DateFormat } from '../../../form/DateFormat';
+
+const NaverLogin = () => {
+  const { naver } = window;
+  const NAVER_CLIENT_ID = process.env.REACT_APP_NAVER_CLIENT_ID;
+  const NAVER_CALLBACK_URL = 'http://localhost:3000/login';
+  const nav = useNavigate();
+  const dispatch = useDispatch();
+  const NaverLoginInit = () => {
+    const naverLogin = new naver.LoginWithNaverId({
+      clientId: NAVER_CLIENT_ID,
+      callbackUrl: NAVER_CALLBACK_URL,
+      isPopup: false,
+      loginButton: { color: 'green', type: 1, height: 60 },
+      callbackHandle: true,
+    });
+    naverLogin.init();
+    const getUser = async () => {
+      await naverLogin.getLoginStatus(async (status) => {
+        if (status) {
+          const userInfo = naverLogin.user.id;
+          dispatch(setUserInfo(naverLogin.user.id));
+          const allList = await getData(userInfo);
+          dispatch(setAllTodoList(allList));
+          const dateFormat = DateFormat(new Date());
+          dispatch(setDate(dateFormat));
+          const todoListForDate = allList.find(
+            (item) => item.date === dateFormat
+          );
+          if (todoListForDate) {
+            dispatch(setTodoList(todoListForDate.todoList));
+          } else {
+            dispatch(setTodoList([]));
+          }
+          nav('/MainPage');
+        }
+      });
+    };
+    getUser();
+  };
+  useEffect(() => {
+    NaverLoginInit();
+  });
+  return (
+    <>
+      <div>
+        <div id="naverIdLogin"></div>
+      </div>
+    </>
+  );
+};
+export default NaverLogin;
